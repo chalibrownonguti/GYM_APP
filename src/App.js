7a1af2bdcbc571b9941f62fdf0f0a0e798d270ec
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-04 17:00:15 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
+import Programs from './components/Hero/Programs/Programs';
+import Reasons from './components/Reasons/Reasons'
+import Testimonials from './components/Testimonials/Testimonials';
+import Join from './components/Join/Join';
+import Footer from './components/Footer/Footer';
+import Members from './components/Members/Members';
+import Trainer from './components/Trainer/Trainer';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+          <Programs/>
+          <Reasons/>
+          <Plans/>
+          <Testimonials/>
+          <Join/>
+          <Footer/>
+          <Members/>
+          <Trainer/>
+         
+        
+          
+    </div>
+  );
+}
+
+export default App;
